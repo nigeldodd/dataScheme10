4e1d5d8a17a295915f41f0a130e4162afd4e9cd2
nigeldodd <nigeldodd@gmail.com>
2021-12-01 13:53:27 +0000

    alert(p1.name)

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function changeColor(newColor) {
   var elem = document.getElementById('para');
   elem.style.color = newColor;
 }
-document.getElementById("pplaceholder").innerText="replacement text"
+document.getElementById("pplaceholder").innerText="this text has been replaced"
 
 /*function replaceHolder() {
   document.getElementById("pplaceholder").innerText="replacement text"
@@ -20,3 +20,8 @@ document.getElementById("pplaceholder").innerText="replacement text"
 
 replaceHolder();*/
 //document.getElementById("pplaceholder").innerHTML="replacement text"
+
+const p1={
+  "name" : "Elon"
+ }
+ alert(p1.name)
