5022787ae7d37ab620fb59e28c79c9f7ad953f89
nigeldodd <nigeldodd@gmail.com>
2021-12-08 20:20:31 +0000

    rowM2.className="stripe"

--- js/main.js
+++ js/main.js
@@ -66,7 +66,7 @@ for (var i = 0; i < ps.length; i++) { //iterate over patients
   var middleTable = document.createElement("table");
   var rowM1 = document.createElement("tr"); //top row for PET, CT etc
   var rowM2 = document.createElement("tr"); //bottom row for stripe
-  //rowM2.className="stripe"
+  rowM2.className="stripe"
   for (var j = 0; j <ps[i].stripe.length; j++) {//all the parts of the stripe
     var cellM1=document.createElement("td");
     var cellM2=document.createElement("td");
@@ -75,7 +75,6 @@ for (var i = 0; i < ps.length; i++) { //iterate over patients
     cellM2.style.width = cellLen + "px";
     cellM1Text=document.createTextNode(ps[i].stripe[j].txt)
     cellM1.appendChild(cellM1Text)
-    //cellM1.className='stripe'
     cellM2.className='grn';
     rowM1.appendChild(cellM1); //append cells one after another in the top row
     rowM2.appendChild(cellM2); //append cells one after another in the bottom row
