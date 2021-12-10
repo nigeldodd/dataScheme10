7a5b1e74d4f0eafaf8d97b2ec7e651d94c4c9e33
nigeldodd <nigeldodd@gmail.com>
2021-12-10 11:17:41 +0000

    rowM2.className='stripe';

--- js/main.js
+++ js/main.js
@@ -106,7 +106,8 @@ const ps2 = [
 const colTable = [{
   'r' : "red",
   'g' : "grn",
-  'b' : "blu"}
+  'b' : "blu"
+  'w' : "whi"}
 ];
 
 /*
@@ -132,25 +133,16 @@ for (var i = 0; i < ps.length; i++) { //iterate over patients
   var middleTable = document.createElement("table");
   var rowM1 = document.createElement("tr"); //top row for PET, CT etc
   var rowM2 = document.createElement("tr"); //bottom row for stripe
-  //rowM2.className='stripe';
+  rowM2.className='stripe';
   for (var j = 0; j <ps[i].stripe.length; j++) {//all the parts of the stripe
     var cellM1=document.createElement("td");
     var cellM2=document.createElement("td");
     var cellLen = ps[i].stripe[j].len;
     cellM1.style.width = cellLen + "px";
     cellM2.style.width = cellLen + "px";
-    //cellM2.style.height = "100px";
     cellM1Text=document.createTextNode(ps[i].stripe[j].txt)
     cellM1.appendChild(cellM1Text)
-    //dummyText=document.createTextNode('..');
-    //cellM2.appendChild(dummyText);
-    //cludge to get tr to be a certain height
-    //since setting class of tr to stripe does not work
-    //var divInTr=document.createElement("div");
-    //divInTr.className='stripe';
-    //cellM2.appendChild(divInTr);
-    cellM2.classList.add('blu', 'stripe');
-    //cellM2.className='blu';
+    cellM2.classList.add('blu');
     rowM1.appendChild(cellM1); //append cells one after another in the top row
     rowM2.appendChild(cellM2); //append cells one after another in the bottom row
   }//end of middle cell
