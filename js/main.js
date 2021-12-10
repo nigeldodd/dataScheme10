80b48eec3081979045cd6df0b6d4c57c2716618c
nigeldodd <nigeldodd@gmail.com>
2021-12-10 11:44:23 +0000

    document.getElementById("pplaceholder").innerText=colRef;

--- js/main.js
+++ js/main.js
@@ -143,7 +143,8 @@ for (var i = 0; i < ps.length; i++) { //iterate over patients
     cellM1Text=document.createTextNode(ps[i].stripe[j].txt);
     cellM1.appendChild(cellM1Text);
     var colRef=colTable[ps[i].stripe[j].col];
-    document.getElementById("pplaceholder").innerText=ps[i].stripe[j].col;
+    //document.getElementById("pplaceholder").innerText=ps[i].stripe[j].col;
+    document.getElementById("pplaceholder").innerText=colRef;
     cellM2.className=colRef;
     rowM1.appendChild(cellM1); //append cells one after another in the top row
     rowM2.appendChild(cellM2); //append cells one after another in the bottom row
