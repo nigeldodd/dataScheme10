dc9e5eae4581498b920d7bbf9959c811e572f5e2
nigeldodd <nigeldodd@gmail.com>
2022-01-17 15:02:27 +0000

    first import

--- js/main.js
+++ js/main.js
@@ -184,152 +184,150 @@ const psTarget = [{
 ]   ; 
 /* Only rule 1 is triggered by Benny Hill
 */
-var ps = []; //Rendering Data Packet, outside of loop so it can contain all patients in CDP
-for (var p = 0; p < PatientDatalist.length; p++){
+
+var ps = []; //Rendering Data Packet List, outside of loop so it can contain all patients in CDP
+var cdp; // Clinical Data Packet (for just one patient)
+var rdp; // Rendering Data Packet (for just one patient)
+for (var p = 0; p < PatientDatalist.length; p++) {
+  cdp = PatientDatalist[p];
+  rdp = parseRdp(cdp);
+  ps.push(rdp);
+}
+function getDateCheck(milestone,dateKey){
+  if(milestone.hasOwnProperty(dateKey)){
+    var thisDate = new Date(milestone[dateKey]);
+    if (thisDate != "Invalid Date") {
+      return(thisDate);
+    }
+  }
+  return(0);
+}
+
+function parseRdp(PatientData) {
   //PatientData is the Clinical Data Packet
-  var PatientData=PatientDatalist[p];
   var referralDateVar = PatientData["referralDate"];
   var referralDateDate = new Date(referralDateVar);
   var lenMilestones = PatientData["milestones"].length;
-  var triageCompletedOn;
-  var thoraxCompletedOn;
-  var petCtCompletedOn;
-  var clinicAddedAt;
-  var ebusAddedAt;
-  var surgeryAddedAt;
-  var surgeryCompletedOn;
   //variables for rule delimiters
-  var rule1Start=referralDateDate;
-  var rule1End=0;
-  var rule2Start=0;
-  var rule2End=0;
-  var rule3Start=0;
-  var rule3End=0;
-  var rule4Start=0;
-  var rule4End=0;
-  var rule1StartD=0;
-  var rule1EndD=0;
-  var rule2StartD=0;
-  var rule2EndD=0;
-  var rule3StartD=0;
-  var rule3EndD=0;
-  var rule4StartD=0;
-  var rule4EndD=0;
+  var rule1Start = referralDateDate;
+  var rule1End = 0;
+  var rule2Start = 0;
+  var rule2End = 0;
+  var rule3Start = 0;
+  var rule3End = 0;
+  var rule4Start = 0;
+  var rule4End = 0;
+  var rule1StartD = 0;
+  var rule1EndD = 0;
+  var rule2StartD = 0;
+  var rule2EndD = 0;
+  var rule3StartD = 0;
+  var rule3EndD = 0;
+  var rule4StartD = 0;
+  var rule4EndD = 0;
   //etc.
   const currentDate = new Date();
-  for (var i = 0; i < lenMilestones; i++){
+  for (var i = 0; i < lenMilestones; i++) {
     var psMilestone = {};
-    var thisMilestone=PatientData["milestones"][i];
-    if(thisMilestone["milestoneType"]["name"]=="Triage"){
-      triageCompletedOn = new Date(thisMilestone["completedOn"]);
-      rule1End = triageCompletedOn;
+    var thisMilestone = PatientData["milestones"][i];
+    if (thisMilestone["milestoneType"]["name"] == "Triage") {
+      rule1End = getDateCheck(thisMilestone,"completedOn");
     }
-    else if(thisMilestone["milestoneType"]["name"]=="CT thorax"){
-      thoraxCompletedOn = new Date(thisMilestone["completedOn"]);
-      rule2Start=thoraxCompletedOn;
+    else if (thisMilestone["milestoneType"]["name"] == "CT thorax") {
+      rule2Start = getDateCheck(thisMilestone,"completedOn");
     }
-    else if(thisMilestone["milestoneType"]["name"]=="clinic"){
-      clinicAddedAt = new Date(thisMilestone["addedAt"]);
-      rule2End=clinicAddedAt;
+    else if (thisMilestone["milestoneType"]["name"] == "clinic") {
+      rule2End = getDateCheck(thisMilestone,"addedAt");
     }
-    else if(thisMilestone["milestoneType"]["name"]=="PET-CT"){
-      petCtCompletedOn = new Date(thisMilestone["completedOn"]);
-      if(petCtCompletedOn != "Invalid Date"){
-       rule3Start=petCtCompletedOn;
-      }
+    else if (thisMilestone["milestoneType"]["name"] == "PET-CT") {
+      rule3Start = getDateCheck(thisMilestone,"completedOn");
     }
-    else if(thisMilestone["milestoneType"]["name"]=="EBUS"){
-      ebusAddedAt = new Date(thisMilestone["addedAt"]);
-      rule3End=ebusAddedAt;
+    else if (thisMilestone["milestoneType"]["name"] == "EBUS") {
+      rule3End = getDateCheck(thisMilestone,"addedAt");
     }
-    else if(thisMilestone["milestoneType"]["name"]=="Surgery"){
-      surgeryAddedAt = new Date(thisMilestone["addedAt"]);
-      rule4Start=surgeryAddedAt;
-      surgeryCompletedOn = new Date(thisMilestone["completedOn"]);
-      rule4End=surgeryCompletedOn;
+    else if (thisMilestone["milestoneType"]["name"] == "Surgery") {
+      rule4Start = getDateCheck(thisMilestone,"addedAt");
+      rule4End = getDateCheck(thisMilestone,"completedOn");
     }
   }
   // convert the dates to days from referral
-  var msDay=1000*60*60*24;
-  rule1StartD=0;
-  rule1EndD=(rule1End-rule1Start)/msDay;
-  rule2StartD=(rule2Start-rule1Start)/msDay;
-  rule2EndD=(rule2End-rule1Start)/msDay;
-  if(rule3Start){
-    rule3StartD=(rule3Start-rule1Start)/msDay;
+  var msDay = 1000 * 60 * 60 * 24;
+  rule1StartD = 0;
+  rule1EndD = (rule1End - rule1Start) / msDay;
+  rule2StartD = (rule2Start - rule1Start) / msDay;
+  rule2EndD = (rule2End - rule1Start) / msDay;
+  if (rule3Start) {
+    rule3StartD = (rule3Start - rule1Start) / msDay;
   }
-  if(rule3End){
-    rule3EndD=(rule3End-rule1Start)/msDay;
+  if (rule3End) {
+    rule3EndD = (rule3End - rule1Start) / msDay;
   }
-  if(rule4Start){
-    rule4StartD=(rule4Start-rule1Start)/msDay;
+  if (rule4Start) {
+    rule4StartD = (rule4Start - rule1Start) / msDay;
   }
-  if(rule4End){
-    rule4EndD=(rule4End-rule1Start)/msDay;
+  if (rule4End) {
+    rule4EndD = (rule4End - rule1Start) / msDay;
   }
 
   //populate timeLine of length 62 with g
   var timeLine = new Array(62).fill("g");
 
-// turn red or blue the days corresponding to the rules
+  // turn red or blue the days corresponding to the rules
   //Rule 1: If “mile” (actually a variable distance), between two milestones is between referral date and triage milestones [completedOn], then colour this red
-  for (var i = rule1StartD; i < rule1EndD; i++){
-    timeLine[i]="r";
+  for (var i = rule1StartD; i < rule1EndD; i++) {
+    timeLine[i] = "r";
   }
   //Rule 2: Else if “mile” is between CT thorax done [completedOn] and clinic requested [addedAt], then colour is red
-  for (var i = rule2StartD; i < rule2EndD; i++){
-    timeLine[i]="r";
+  for (var i = rule2StartD; i < rule2EndD; i++) {
+    timeLine[i] = "r";
   }
   //Rule 3: Else if “mile is between PET-CT [completedOn] and EBUS requested [addedAt], then colour is red
-  for (var i = rule3StartD; i < rule3EndD; i++){
-    timeLine[i]="r";
+  for (var i = rule3StartD; i < rule3EndD; i++) {
+    timeLine[i] = "r";
   }
   //Rule 4: Else if “mile” is between surgery requested and surgery has not yet been done, then colour is blue
-  for (var i = rule4StartD; i < rule4EndD; i++){
-    timeLine[i]="b";
+  for (var i = rule4StartD; i < rule4EndD; i++) {
+    timeLine[i] = "b";
   }
   //Remainder of 62 day period is coloured white
-  if(rule4StartD){ // to guard against overwriting everything white
-    for (var i = rule4EndD; i < 62; i++){
-      timeLine[i]="w";
+  if (rule4StartD) { // to guard against overwriting everything white
+    for (var i = rule4EndD; i < 62; i++) {
+      timeLine[i] = "w";
     }
   }
 
-//build the array "count" to contain the run lenghts of the colours
-  var j=0;
+  //build the array "count" to contain the run lenghts of the colours
+  var j = 0;
   var count = [];
-  for(var i =0; i<62 ; i++){
-    count[j]=0;
-    var t1=timeLine[i];
-    while (timeLine[i]==t1){
-      count[j]=count[j]+1;
+  for (var i = 0; i < 62; i++) {
+    count[j] = 0;
+    var t1 = timeLine[i];
+    while (timeLine[i] == t1) {
+      count[j] = count[j] + 1;
       i++;
     }
     i--;
     j++;
   }
 
-//build the psMilestones data structure that will become part of the RDP
+  //build the psMilestones data structure that will become part of the RDP
   var scaleFac = 10;//for display. Should eventually be scaled such that 62 days fills the screen.
-  var psPatientData = {"name" : PatientData["firstName"] + " " + PatientData["lastName"]} //Rendering Data Packet element
+  var psPatientData = { "name": PatientData["firstName"] + " " + PatientData["lastName"] } //Rendering Data Packet element
   var psMilestones = []; //list of {"col" : "b","len":166, "txt":"MDT"} type elements. i.e. RDP element without the header
-  var colIndex=0;
-  for (var i=0; i<count.length; i++){
+  var colIndex = 0;
+  for (var i = 0; i < count.length; i++) {
     var psMilestone = {};
-    psMilestone.col=timeLine[colIndex];
-    psMilestone.len=count[i]*scaleFac;
+    psMilestone.col = timeLine[colIndex];
+    psMilestone.len = count[i] * scaleFac;
     colIndex += count[i];//do this here to get the txt to equal the day number
-    psMilestone.txt=colIndex;
-    //psMilestone.txt="";
+    psMilestone.txt = colIndex;
     psMilestones.push(psMilestone)
   }
 
-  psPatientData.stripe=psMilestones;
-  ps.push(psPatientData);//so that this psPatientData element becomes an element of the RDP
+  psPatientData.stripe = psMilestones;
+  return (psPatientData);
+}// end of function parseRDP
 
-//Now we have ps resembling psTarget in all but txt
-
-}// end of for (var p = 0; p < PatientDatalist.length){
 //Now we have ps resembling psTarget in all but txt
 
 //Now we have ps resembling psTarget in all but txt
@@ -407,5 +405,5 @@ for (var i = 0; i < ps.length; i++) { //iterate over patients
   ourTableBody.appendChild(row)
 }
 ourTable.appendChild(ourTableBody);//outer table placed in ourTableBody which is passed from html
-document.getElementById("pplaceholder").innerText="rev 004"; //useful for debugging
+document.getElementById("pplaceholder").innerText="rev 001"; //useful for debugging
 
