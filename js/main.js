fb2f218efaf1b790bf47107c7e7777ac4c579138
nigeldodd <nigeldodd@gmail.com>
2022-01-22 17:16:17 +0000

    ""

--- js/main.js
+++ js/main.js
@@ -413,7 +413,8 @@ for (var i = 0; i < ps.length; i++) { //iterate over patients
     var cellLen = ps[i].stripe[j].len - 4;
     cellM1.style.width = cellLen + "px";
     cellM2.style.width = cellLen + "px";
-    cellM1Text=document.createTextNode(ps[i].stripe[j].txt);
+    //cellM1Text=document.createTextNode(ps[i].stripe[j].txt);
+    cellM1Text="";
     cellM1.appendChild(cellM1Text);
     // Lookup the colour and set the css style
     var colKey = ps[i].stripe[j].col; //colKey is a string
@@ -438,5 +439,5 @@ for (var i = 0; i < ps.length; i++) { //iterate over patients
   ourTableBody.appendChild(row)
 }
 ourTable.appendChild(ourTableBody);//outer table placed in ourTableBody which is passed from html
-document.getElementById("pplaceholder").innerText="rev 011"; //useful for debugging
+document.getElementById("pplaceholder").innerText="rev 012"; //useful for debugging
 
